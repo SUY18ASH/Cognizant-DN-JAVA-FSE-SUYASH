2ada4e50f0b29876dfad8a433216406b0c69e780
Suyash Chouksey <suyashchouksey2004@gmail.com>
2025-07-14 00:59:43 +0530

    Add files via upload

---
+++ WEEK4/Exercise4/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,24 @@
+// src/main/java/com/cognizant/springlearn/controller/CountryController.java
+package com.cognizant.springlearn.controller;
+
+import com.cognizant.springlearn.Country;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@RestController
+public class CountryController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+        LOGGER.info("START getCountryIndia");
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country india = context.getBean("country", Country.class);
+        LOGGER.info("END getCountryIndia");
+        return india;
+    }
+}
